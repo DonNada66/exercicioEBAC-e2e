a5e075f5b7d81338721cfe2c837b88e79f94bb67
Gabriel Donada <gabrieldonada471@gmail.com>
2025-12-19 15:09:50 -0300

    Primeiro commit

---
+++ cypress/support/page_objects/produtos.page.js
@@ -0,0 +1,32 @@
+class produtosPage {
+
+    visitarUrl() {
+        cy.visit('produtos')
+    }
+
+    buscarProdutos(nomeProduto) {
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+    }
+
+    buscarProdutoLista(nomeProduto) {
+        cy.get('.product-block')
+            .contains(nomeProduto)
+            .click()
+    }
+
+    visitarProduto(nomeProduto) {
+        //cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatado = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatado}`)
+    }
+
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-'+tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
+}
+
+export default new produtosPage()
