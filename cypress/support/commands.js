f9a1f73787753b4422265081ad59009939c193fb
Gabriel Donada <gabrieldonada471@gmail.com>
2026-02-10 09:15:14 -0300

    Atualizado gitignore

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,10 +2,8 @@ Cypress.Commands.add('login', () => {
     const username = Cypress.env('USER_LOGIN');
     const password = Cypress.env('USER_PASSWORD');
 
-    cy.session([username, password], () => {
     cy.get('#username').type(username)
     cy.get('#password').type(password, { log: false })
     cy.get('.woocommerce-form > .button').click()
-    })
 });
 
